59c92e6a1e00fd6e132a2cbc3130d29bb77d4ea3
gscamurra <giovanni.scamurra.adobe@gmail.com>
2024-09-11 16:29:54 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -73,7 +73,7 @@ async function loadFonts() {
 function buildAutoBlocks(main) {
   try {
     
-    var pageTheme = main.querySelector("meta[name='theme']").getAttribute("content");
+    var pageTheme = document.querySelector("meta[name='theme']").getAttribute("content");
     console.log(pageTheme);
 
     buildHeroBlock(main);
